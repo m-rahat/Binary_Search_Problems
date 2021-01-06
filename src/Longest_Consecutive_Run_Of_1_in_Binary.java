df7a219075cd6baea2bf25c3a2ed8b2c2ba7031d
m-rahat <56198714+m-rahat@users.noreply.github.com>
2021-01-06 21:31:37 +0600

    Works 100%
    faster than 38.80% in Java

--- src/Longest_Consecutive_Run_Of_1_in_Binary.java
+++ src/Longest_Consecutive_Run_Of_1_in_Binary.java
@@ -9,37 +9,28 @@ public class Longest_Consecutive_Run_Of_1_in_Binary {
 		int maxLength = 0;
 
 		for (int i = 0; i < num.length(); i++) {
-			String str = "";
 			if (num.charAt(i) == '1') {
 				temp++;
-				if (temp > maxLength) {
-					maxLength = temp;
-					temp = 0;
-					continue;
-				}//if
-			}//if
-			else if (num.charAt(i) == '0') {
-				if (temp > maxLength) {
-					maxLength = temp;
-					temp = 0;
-				}//if
-			}//else if
-
-			if (i == num.length() - 1) {
-				if (temp >= maxLength) {
-					maxLength = temp;
-					temp = 0;
-				}
 			}
-			
+			else if (num.charAt(i) == '0' && temp >= maxLength) {
+				maxLength = temp;
+				temp = 0;
+			}//else if
+			else {
+				temp = 0;
+			}
 		} // for
 
+		if (temp != 0 && temp >= maxLength) {
+			maxLength = temp;
+		}//if
+		
 		return maxLength;
 	}// solve
 
 	public static void main(String[] args) {
 
-		System.out.println(solve(156));
+		System.out.println(solve(36772731));
 
 	}// main
 }// classname
