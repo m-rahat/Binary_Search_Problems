ed7648d1c6b953fb2a68bc116f069916d10b375d
m-rahat <56198714+m-rahat@users.noreply.github.com>
2021-01-06 22:26:51 +0600

    works
    faster than 91.65% in Java

---
+++ src/Invert_Tree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Tree {
+	int val;
+	Tree left;
+	Tree right;
+}// Tree
+
+public class Invert_Tree {
+
+	public static Tree solve(Tree root) {
+
+		Tree tempRoot = new Tree();
+		
+		if (root == null) return null;
+		
+		tempRoot.left = solve(root.right);
+		tempRoot.right = solve(root.left);
+		
+		return tempRoot;
+	}// solve
+
+	public static void main(String[] args) {
+
+	}// main
+}// className
